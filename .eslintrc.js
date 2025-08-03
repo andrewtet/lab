9a5043171d4f216d19f7885e5c5a3191c4c86045
Andy Tetnowski <atetnowski@gmail.com>
2025-08-02 21:36:02 -0700

    a

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,39 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-  ],
-  rules: {
-    // Add any custom rules here
+  root: true,
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  ignorePatterns: ["!**/.server", "!**/.client"],
+
+  plugins: ["unused-imports"],
+  rules: {
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "no-console": ["error", { allow: ["warn", "error"] }],
+  },
+
+  overrides: [
+    {
+      files: ["**/*.{ts,tsx}"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
 };
